d03f5373cdccb363456ffce4c5f91e5cf54112cc
LY <1779894177@qq.com>
2021-04-13 16:31:03 +0800

    2021-4.13leetcode部分进行提交

--- src/main/java/com/xdu/leetcode/tree/LowestCommonAncestor.java
+++ src/main/java/com/xdu/leetcode/tree/LowestCommonAncestor.java
@@ -3,7 +3,7 @@ package com.xdu.leetcode.tree;
 
 
 /**
- *二叉树的最近公共祖先
+ *二叉树的最近公共祖先（这个树是普通的二叉树，所以用递归的方法）
  *给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
  *
  * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。
@@ -23,13 +23,23 @@ package com.xdu.leetcode.tree;
   }
 public class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null; // 如果树为空，直接返回null
-        if(root == p || root == q) return root; // 如果 p和q中有等于 root的，那么它们的最近公共祖先即为root（一个节点也可以是它自己的祖先）
-        TreeNode left = lowestCommonAncestor(root.left, p, q); // 递归遍历左子树，只要在左子树中找到了p或q，则先找到谁就返回谁
-        TreeNode right = lowestCommonAncestor(root.right, p, q); // 递归遍历右子树，只要在右子树中找到了p或q，则先找到谁就返回谁
-        if(left == null) return right; // 如果在左子树中 p和 q都找不到，则 p和 q一定都在右子树中，右子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
-        else if(right == null) return left; // 否则，如果 left不为空，在左子树中有找到节点（p或q），这时候要再判断一下右子树中的情况，如果在右子树中，p和q都找不到，则 p和q一定都在左子树中，左子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
-        else return root; //否则，当 left和 right均不为空时，说明 p、q节点分别在 root异侧, 最近公共祖先即为 root
+        if(root == null) return null;
+        // 如果树为空，直接返回null
+        if(root == p || root == q) return root;
+        // 如果 p和q中有等于 root的，那么它们的最近公共祖先即为root（一个节点也可以是它自己的祖先）
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // 递归遍历左子树，只要在左子树中找到了p或q，则先找到谁就返回谁
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 递归遍历右子树，只要在右子树中找到了p或q，则先找到谁就返回谁
+        if(left == null)
+            return right;
+        // 如果在左子树中 p和 q都找不到，则 p和 q一定都在右子树中，右子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+        else if(right == null)
+            return left;
+        // 否则，如果 left不为空，在左子树中有找到节点（p或q），这时候要再判断一下右子树中的情况，如果在右子树中，p和q都找不到，则 p和q一定都在左子树中，左子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+        else
+            return root;
+        //否则，当 left和 right均不为空时，说明 p、q节点分别在 root异侧, 最近公共祖先即为 root
     }
 
 
